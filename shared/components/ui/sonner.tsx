88b8e40abc0ad29f98f7cb4a43e41e4c4ad6471f
Riikta3 <tarik.klezo@gmail.com>
2026-02-02 23:26:28 +0100

    chore: Update the dev server port to 3003 and remove custom classNames from Sonner toast options.

--- shared/components/ui/sonner.tsx
+++ shared/components/ui/sonner.tsx
@@ -13,17 +13,6 @@ export function Toaster({ ...props }: ToasterProps) {
       theme={theme as ToasterProps["theme"]}
       className='toaster group'
       richColors
-      toastOptions={{
-        classNames: {
-          toast:
-            "group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-950 group-[.toaster]:border-gray-200 group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-gray-500",
-          actionButton:
-            "group-[.toast]:bg-gray-900 group-[.toast]:text-gray-50",
-          cancelButton:
-            "group-[.toast]:bg-gray-100 group-[.toast]:text-gray-500",
-        },
-      }}
       {...props}
     />
   );
